6e30c71e523a2e9791f8421949ec169f9fd6e24b
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2020-05-09 00:53:33 +0200

    Add color pick to choose toolColor

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
 let userCanvas
 let canvas
+let toolColor
+let colorPicker
 
 function setup() {
     userCanvas = document.querySelector('canvas')
     canvas= createCanvas(userCanvas.width, userCanvas.height)
     canvas.position(userCanvas.offsetLeft, userCanvas.offsetTop)
+    colorPicker = createColorPicker('#ff0000');
+    colorPicker.position(10, 10);
 }
 
 function draw() {
@@ -12,15 +16,17 @@ function draw() {
         resizeCanvas(userCanvas.width, userCanvas.height)
     }
     clear()
+    toolColor = colorPicker.color()
     drawCursor(mouseX, mouseY)
+
 }
 
 function drawCursor(x, y) {
     if(x > width || y > height) return 
-    stroke(0)
+    stroke(toolColor)
     circle(x, y, 7)
     line(0, y, width, y)
     line(x, 0, x, height)
-    fill(0)
+    fill(toolColor)
     text(`(${x}, ${y})`, x + 5, y - 5)
 }
